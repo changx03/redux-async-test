0484aabcd53db979a943d9cc9b3220585ef87afe
changx03 <luke.x.chang@gmail.com>
2018-03-15 22:24:30 +1300

    reduce clean up

--- src/store/reducerResult.js
+++ src/store/reducerResult.js
@@ -1,27 +1,29 @@
 import actionTypes from './actionTypes';
+import { updateObject } from './utility';
 
 const initialState = {
   results: [],
 };
 
+const storeResult = (state, action) => {
+  return updateObject(state, {
+    results: state.results.concat({ id: new Date(), value: action.result }),
+  });
+};
+
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter(
+    result => result.id !== action.resultElId
+  );
+  return updateObject(state, { results: updatedArray });
+};
+
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({ id: new Date(), value: action.result }),
-      };
+      return storeResult(state, action);
     case actionTypes.DELETE_RESULT:
-      // const id = 2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      const updatedArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return {
-        ...state,
-        results: updatedArray,
-      };
+      return deleteResult(state, action);
     default:
       return state;
   }
